0f1c5997221596c6eb7220c026f9f631b1ee22d1
gdm <gdm@geDem.lan>
2024-05-11 18:20:38 +0900

    Googleサチコ導入の修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description:
     "現役エンジニアによるテックブログです。主にフロントエンド・モバイル(iOS/Android)を中心に、機能の実装方法・デバッグ方法を紹介します。",
   verification: {
-    google: process.env["GOOGLE_SITE_VERIFICATION_CODE"],
+    google: process.env.GOOGLE_SITE_VERIFICATION_CODE,
   },
 };
 
