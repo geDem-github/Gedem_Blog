b6af45b418886b5c220ba79e2ed93597ff7b8307
gdm <gdm@geDem.lan>
2024-05-19 09:53:16 +0900

    word-breakを追加

--- src/app/_components/post/headings.tsx
+++ src/app/_components/post/headings.tsx
@@ -5,7 +5,7 @@ type Props = {
 const Headings = (props: Props) => {
   return (
     <div className="border bg-slate-700 rounded-xl flex flex-col justify-center px-4 py-6">
-      <div className="">
+      <div>
         <h2 className="text-3xl font-bold mb-4 md:text-2xl">もくじ</h2>
         <ul>
           {props.headings.map((text, index) => {
