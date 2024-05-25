57ec6a59fd03b4a1f74da65dc409e4bfc0f8863e
gdm <gdm@geDem.lan>
2024-05-25 09:13:06 +0900

    Google Analytics導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
+import { env } from "process";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +26,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+        `,
+        }}
+      />
+
       <body className={inter.className}>
         {children}
         <Analytics />
